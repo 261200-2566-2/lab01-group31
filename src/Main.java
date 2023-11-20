7407beb6c13f8e4a6d2c66acad697f45043490c0
lab 23 <kanonlas_r@cmu.ac.th>
2023-11-20 23:59:26 +0700

    complete version 1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,82 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void AscendingOrderByUsingBubble(int[] arr){
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+//        int value;
+//        for (int i = 0; i < my.length; i++) {
+//            for (int j = k + 1; l < my.length; l++) {
+//                if (my[k] > my[l]) {
+//                    value = my[k];
+//                    my[k] = my[l];
+//                    my[l] = value;
+//                }
+//            }
+//        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("This is bubble sort >>>");
+    }
+
+    public static void AscendingOrderByUsingInsertion(int[] my){
+        int n = my.length;
+        for (int i = 1; i < n; ++i) {
+            int key = my[i];
+            int j = i - 1;
+
+            while (j >= 0 && my[j] > key) {
+                my[j + 1] = my[j];
+                j = j - 1;
+            }
+            my[j + 1] = key;
+        }
+        System.out.println("This is insertion sort >>>");
+    }
+
+
+
+    public static void Print(int [] my){
+        for (int k : my) System.out.println(k);
+    }
+
+
+
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int number = s.nextInt();
+
+        if(number < 0) System.out.println("error");
+        else {
+            int[] my = new int[number];
+            for (int i = 0; i < my.length; i++) {
+                System.out.println("Enter your number value: ");
+                my[i] = s.nextInt();
+            }
+
+            AscendingOrderByUsingBubble(my);
+            Print(my);
+            AscendingOrderByUsingInsertion(my);
+            Print(my);
         }
     }
 }
+
+
+/* reference : https://beginnersbook.com/2018/10/java-program-to-sort-an-array-in-ascending-order/
+               https://stackoverflow.com/questions/7787754/sorting-integers-in-order-lowest-to-highest-java
+               https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html#digit-char-i
+               https://docs.oracle.com/javase/8/docs/api/java/lang/Integer.html#compareUnsigned-int-int-
+               https://sentry.io/answers/how-do-i-convert-a-string-to-an-int-in-java/
+
+*/
+
